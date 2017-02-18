739d0e4e4221ea04d470f8081674514c40951bc1
Alicia Li <alicial@microsoft.com>
2017-02-17 17:17:18 -0800

    add wcf docker container samples

---
+++ 4.6.2/WcfBasicHttpSelfhost/WCFClientBasic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WCFClientBasic.ServiceReference1;
+
+namespace WCFClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client wcfClient = new Service1Client();
+            string retstr = wcfClient.GetData(123);
+            Console.WriteLine("GetData returns: " + retstr);
+            CompositeType obj = new CompositeType();
+            obj.BoolValue = true;
+            obj.StringValue = "Hello WCF client";
+            CompositeType objret = wcfClient.GetDataUsingDataContract(obj);
+            Console.WriteLine("GetDataUsingDataContract returns: " + objret.StringValue);
+            Console.ReadLine();
+            wcfClient.Close();
+        }
+    }
+}
